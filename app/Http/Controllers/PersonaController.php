e61948025450ebdd76a2edc46c7db6cdcf19fc10
KitsukiPho3nix <gusferreira1212@gmail.com>
2024-07-20 14:05:20 -0300

    Creo un controlador y un modelo mediante php artisan make:model Persona -cr para la tabla Personas de mi base de datos, para comenzar con la API.

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller {
+    public function Crear() {
+        //
+    }
+    public function Listar() {
+        //
+    }
+    public function ListarUna() {
+        //
+    }
+    public function Modificar() {
+        //
+    }
+    public function Eliminar() {
+        //
+    }
+}
