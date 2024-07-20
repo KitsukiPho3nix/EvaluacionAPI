748517469fa51b9976a61becf1b793c0fc3dabd4
KitsukiPho3nix <gusferreira1212@gmail.com>
2024-07-20 14:48:49 -0300

    Creo el endpoint y lógica para dar de alta a nuevas personas (Crear). Ya quedó completamente funcional esta opción también, mediante el método POST.

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -4,8 +4,14 @@ use App\Models\Persona;
 use Illuminate\Http\Request;
 
 class PersonaController extends Controller {
-    public function Crear() {
-        //
+    public function Crear(Request $request) {
+        $persona = new Persona();
+        $persona -> nombre = $request->post('nombre');
+        $persona -> apellido = $request->post('apellido');
+        $persona -> telefono = $request->post('telefono');
+        $persona -> save();
+
+        return $persona;
     }
     public function Listar() {
         return Persona::all();
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@ use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/persona', [PersonaController::class, 'Crear']);
 Route::get('/persona', [PersonaController::class, 'Listar']);
 Route::get('/persona/{d}', [PersonaController::class, 'ListarUna']);
